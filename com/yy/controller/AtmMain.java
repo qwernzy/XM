d676a70212d62c7ca017ae2f4ff93b016fd0fb8d
n <1715260699@qq.com>
2021-07-12 12:03:37 +0800

    atm机项目和test递归删除

--- com/yy/controller/AtmMain.java
+++ com/yy/controller/AtmMain.java
@@ -14,7 +14,7 @@ import static com.yy.service.UserService.gn;
 public class AtmMain {
     private static String id;
     private static String pwd;
-     private UserService us=new UserService();
+
 
     public static void main(String[] args) {
 
@@ -26,15 +26,11 @@ public class AtmMain {
            TextUtil.huanying();
            User ut=UserService.inputKM();
            System.out.println("ut = " + ut);
-           i++;
+
            if (i > 5) {
+               System.out.println("输入密码错误次数大于五次，您的银行卡已被封禁");
                 break;
            }
-           for (User user : users) {
-               System.out.println("user = " + user);
-           }
-
-
            for (int c=0;c<users.size();c++) {
 
                if (users.get(c).getId().equals(ut.getId()) && users.get(c).getPwd().equals(ut.getPwd())) {
@@ -44,12 +40,16 @@ public class AtmMain {
 
                        TextUtil.yhjm();
                        System.out.println("您的余额为 " + users.get(c).getQian()+"\n请输入功能序号");
-                       boolean asd=gn(c);
+                       boolean asd=true;
                        if (asd==false) {
                            break;
+                       }else {
+                           gn(c);
                        }
                    }
 
+               }else{
+                   i++;
                }
            }
 
@@ -61,7 +61,7 @@ public class AtmMain {
 
 
     private static void d(@NotNull String id,String pwd){
-        if (id.length() ==6 ) {
+        if (id.length() ==8 ) {
 
         }
     }
--- com/yy/service/UserService.java
+++ com/yy/service/UserService.java
@@ -49,52 +49,92 @@ public class UserService {
 
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        for (User user : users) {
-            System.out.println("user = " + user);
-        }
+
+
+        return fz(c, users, sc, s);
+
+    }
+
+    private static boolean fz(int c, List<User> users, Scanner sc, String s) {
 
         switch (s) {
             case "1":
 
-                System.out.println("你想取多少钱？\n请输入数额（输入整数，我吐不出硬币）");
-                int ss = sc.nextInt();
+                quqian(c, users, sc);
 
-                users.get(c).setQian(users.get(c).getQian()-ss);
-                System.out.println(users.get(c).getQian());
                 return true;
             case "2":
-                System.out.println(" 你想给谁转账？");
-                String card = sc.nextLine();
-                System.out.println(" 转多少钱？ ");
-                int q=sc.nextInt();
-                System.out.println("card = " + card);
-
-                for (int caid = 0; caid < users.size() ; caid++) {
-                    if (card.equals(users.get(caid).getId())) {
-                        users.get(c).setQian(users.get(c).getQian()-q);
-                        users.get(caid).setQian(users.get(caid).getQian()+q);
-                        System.out.println("转账成功");
-                        return true;
-                    }
-                }
+                zhuanzhang(c, users, sc);
                 return true;
             case "3":
-                System.out.println("存钱");
-                System.out.println(" 存多少钱？ ");
-                int qadd=sc.nextInt();
-                users.get(c).setQian(users.get(c).getQian()+qadd);
+                cunqian(c, users, sc);
                 return true;
             case "4":
                 System.out.println("退卡");
                 return false;
             case "520":
-
+                bianli(users);
+                return true;
             default:
-
-
                 System.out.println("请输入正确的功能序号！");
                 return true;
         }
+    }
 
+    private static void bianli(List<User> users) {
+        for (User user : users) {
+            System.out.println("user = " + user);
+        }
+    }
+
+    private static void cunqian(int c, List<User> users, Scanner sc) {
+        System.out.println("存钱");
+        System.out.println(" 存多少钱？ (存入最小金额100元存款金额应为100的倍数小额面值将会被当做假币吞入)");
+        int qadd=sc.nextInt()%100;
+        users.get(c).setQian(users.get(c).getQian()+qadd);
+    }
+
+    private static void zhuanzhang(int c, List<User> users, Scanner sc) {
+        System.out.println(" 你想给谁转账？");
+        String card = sc.nextLine();
+        System.out.println(" 转多少钱？ ");
+        double q=sc.nextDouble();
+        if (q > users.get(c).getQian()) {
+            for (int caid = 0; caid < users.size() ; caid++) {
+                if (card.equals(users.get(caid).getId())) {
+                    users.get(c).setQian(users.get(c).getQian()-q);
+                    users.get(caid).setQian(users.get(caid).getQian()+q);
+                    System.out.println("转账成功");
+
+
+
+                }
+            }
+        }else {
+            System.out.println("您的账户余额不足，您的账户余额为："+users.get(c).getQian());
+
+        }
+
+
+
+
+
+    }
+
+    private static void quqian(int c, List<User> users, Scanner sc) {
+        System.out.println("你想取多少钱？\n请输入数额（输入整数，我吐不出硬币）");
+        int ss = sc.nextInt();
+        if (ss  > users.get(c).getQian()) {
+            users.get(c).setQian(users.get(c).getQian()-ss);
+            System.out.println(users.get(c).getQian());
+        }else {
+            System.out.println("您的账户余额不足，您的账户余额为："+users.get(c).getQian());
+        }
+
+//        if (ss  > users.get(c).getQian()) {
+//
+//        }else {
+//                      System.out.println("您的账户余额不足，您的账户余额为："+users.get(c).getQian());
+//        }
     }
 }
